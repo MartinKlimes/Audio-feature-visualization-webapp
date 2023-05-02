787e0345f90f4de0bc4a9ac28c151e2e15800f8f
MatePC <xklime44@vutbr.cz>
2023-05-02 15:09:04 +0200

    "Database"

--- frontend/src/composables/custom.js
+++ frontend/src/composables/custom.js
@@ -3,7 +3,7 @@ import { getCookie } from "./cookieHandling";
 
 export const api = axios.create({
   withCredentials: true,
-  baseURL: "http://127.0.0.1:5000/",
+  baseURL: "http://audio-feature-visualization.onrender.com/",
 });
 
 export const updateRecording = (record_id, column, new_value, visualization_type) => {
