92bb8e48452bc2c8ebe176c9a810590e0ba4784d
MatePC <xklime44@vutbr.cz>
2023-05-03 08:32:44 +0200

    d

--- backend/mos_backend/routes_parameters.py
+++ backend/mos_backend/routes_parameters.py
@@ -1,7 +1,7 @@
 from mos_backend import app, db
 from werkzeug.utils import secure_filename
 import os
-import librosa
+import librosa.display
 
 from flask_jwt_extended import jwt_required, current_user
 from flask import request, jsonify
