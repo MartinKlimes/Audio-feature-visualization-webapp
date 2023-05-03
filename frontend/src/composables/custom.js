5a3471859194d9646300fb6a14615a95151b53c2
MatePC <xklime44@vutbr.cz>
2023-05-03 08:35:46 +0200

    d

--- frontend/src/composables/custom.js
+++ frontend/src/composables/custom.js
@@ -7,8 +7,7 @@ export const api = axios.create({
 });
 
 export const updateRecording = (record_id, column, new_value, visualization_type) => {
-i
-    .put(
+api.put(
       "/update-recording",
       {
         record_id: record_id,
